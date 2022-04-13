9f5014bdc0373580b2248f755998494d4a32db86
Ashley Ung <62251927+ung8005@users.noreply.github.com>
2022-04-13 12:16:22 -0700

    reading in more than 1 file

--- delete.py
+++ delete.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python3
+
+# This code reads a density file in text mode, and parses the water density
+
 import numpy as np
 import matplotlib.pyplot as plt 
 import scipy 
 import os
 from scipy import optimize 
 
-# Absolute path to folder in which the files reside 
-folderPath= "/Users/ashleyung/Desktop/Testing/DensityFiles/"
-# Change the directory 
-os.chdir (folderPath)
-# Read text file
-def readDensityData (file_path):
-	with open (file_path, 'r') as f:
-		print (f.read ())
-# Iterate through all files 
-for file in os.listdir ():
-	if file.endswith (".txt"):
-		file_path = f"{path}\{file}"
-		read_text_file (file_path)
+# Absolute path to folder in which the files of analysis reside 
+folderPath = "/Users/ashleyung/Desktop/Testing/DensityFiles/DDC100"
+filePaths = [os.path.join (folderPath, name) for name in os.listdir (folderPath)]
+allFiles = []
+
+# Iterate through all files so now allFiles[0] holds the first file loaded, allFiles[1] holds the second, etc..
+for path in filePaths:
+	with open (path, 'r') as f: 
+		densityFile = f.readlines ()
+		allFiles.append (densityFile)
+# checking 
+print (allFiles[1])
+print ("secondfile")
+print (allFiles[2])
